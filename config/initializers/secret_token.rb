372857296d49be9b030b59c6b4aba2dfbd12b911
Cole Busby <colebusby@gmail.com>
2014-03-17 15:05:41 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ironwood::Application.config.secret_key_base = 'cb900b78c94f8b8b7aaa34c3525c5eae241ecab2319a77669b3026891bcaa593655de0730796aeb24ba805a48185b4754bbae6487734f3de50c4cec08555cfb5'
