990bba7185ddf8fe89a38e47921701fbe8ccfe59
Cole Busby <colebusby@gmail.com>
2014-06-13 09:19:16 -0600

    redo of home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613041712) do
+ActiveRecord::Schema.define(version: 20140613145938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,10 @@ ActiveRecord::Schema.define(version: 20140613041712) do
   end
 
   create_table "board_positions", force: true do |t|
-    t.integer  "resident_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.integer  "resident_id"
   end
 
   create_table "reports", force: true do |t|
