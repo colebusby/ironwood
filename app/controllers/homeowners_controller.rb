27e4911c2c0e780b3b1afe3257f93c28bec21bc0
Cole Busby <colebusby@gmail.com>
2014-08-26 07:45:30 -0600

    Units added to directory

--- app/controllers/homeowners_controller.rb
+++ app/controllers/homeowners_controller.rb
@@ -3,8 +3,11 @@ class HomeownersController < ApplicationController
   def index
     users_last_name = User.directory_last_name_sort
     users_address = User.order('address')
+    users_unit = User.order('unit')
     if params[:last_name]
       @users = users_last_name
+    elsif params[:unit_order]
+      @users = users_unit
     else
       @users = users_address
     end
