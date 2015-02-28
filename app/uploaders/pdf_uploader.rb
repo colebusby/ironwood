b71c946508dd7efdfee6ed1b10316b7af31b6221
Cole Busby <colebusby@gmail.com>
2015-02-28 13:29:45 -0700

    removed path thing from pdf uploader

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -1,7 +1,4 @@
 class PdfUploader < CarrierWave::Uploader::Base
   storage :fog
 
-  def store_path(for_file = filename)
-    self.model.class.name.underscore.pluralize + (version_name ? "/" + version_name.to_s : '') + "/" + "#{model.id}.pdf"
-  end
 end
