615cfe61a4a788857bfc2b44354ae3ac324ab0d6
Cole Busby <colebusby@gmail.com>
2015-02-28 11:58:56 -0700

    added picture create method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,4 +3,21 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
     @picture = Picture.new
   end
+
+  def create
+    picture = Picture.new(picture_params)
+    if picture.save
+      flash[:success] = "Picture uploaded"
+      redirect_to pictures_path
+    else
+      flash[:error] = "Upload failed"
+      render pictures_path
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:name, :image_file)
+  end
 end
