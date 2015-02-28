ba72cf9c178d530108c3f19d3d44f7afe64e6a85
Cole Busby <colebusby@gmail.com>
2015-02-28 12:38:35 -0700

    removed west region from s3 config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,6 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_KEY'],                        # required
       :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
-      :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
       :s3_endpoint            => 's3.amazonaws.com'
     }
     config.fog_directory  = ENV['PICTURES_BUCKET']                     # required
