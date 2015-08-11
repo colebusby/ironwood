c6871aa354bea7b77bbb68870ae9460e94910a0e
Cole Busby <colebusby@gmail.com>
2015-08-11 11:54:19 -0600

    fixed another stupid syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   def self.by_street
     self.all.sort_by do |user|
       street_number = user.address.scan(/\d+/).last
-      street_number = "0" = street_number if street_number.length < 4
+      street_number = "0" + street_number if street_number.length < 4
       street_number + user.address.last
     end
   end
