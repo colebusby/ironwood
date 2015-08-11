448991a995445d14a771addba4ea7082caa6396d
Cole Busby <colebusby@gmail.com>
2015-08-11 12:50:59 -0600

    made address sort smarter

--- app/controllers/homeowners_controller.rb
+++ app/controllers/homeowners_controller.rb
@@ -2,11 +2,14 @@ class HomeownersController < ApplicationController
   before_action :authenticate_user!
   def index
     if params[:last_name]
+      session[:sort] = "name"
       @users = User.directory_last_name_sort
     elsif params[:unit_order]
+      session[:sort] = "unit"
       @users = User.order('unit')
     else
-      session[:address] = !session[:address] if params[:address]
+      session[:address] = !session[:address] if session[:sort] == "address"
+      session[:sort] = "address"
       session[:address] ? @users = User.order('address') : @users = User.by_street
     end
   end
