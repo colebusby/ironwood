7d044c9bc6c6176acd0495ebec06fd97926f5d3c
Cole Busby <colebusby@gmail.com>
2016-09-18 17:27:58 -0600

    changed address sort logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   end
 
   def self.by_street
-    self.all.sort_by do |user|
+    self.where.not(address: nil).sort_by do |user|
       street_number = user.address.scan(/\d+/).last
       street_number = "0" + street_number if street_number.length < 4
       street_number + user.address.last
